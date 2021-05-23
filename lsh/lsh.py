00f982893c5fb63f625ea43daf7e736081447868
pdoyle5000 <pdoyle5000@gmail.com>
2021-05-23 16:04:34 -0400

    added ability to inject planes

--- lsh/lsh.py
+++ lsh/lsh.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Optional, List, Any
 from collections import OrderedDict
 import numpy as np
 
@@ -8,14 +8,24 @@ def euclidean_dist(x: List, y: List) -> float:
 
 
 class LSH:
-    def __init__(self, hash_byte_len: int, num_hashes: int, num_tables: int):
+    def __init__(
+        self,
+        hash_byte_len: int,
+        num_hashes: int,
+        num_tables: int,
+        planes: Optional[List[np.ndarray]] = None,
+    ):
         self.hash_byte_len = hash_byte_len
         self.num_hashes = num_hashes
         self.num_tables = num_tables
 
-        self.planes = [
-            np.random.randn(self.hash_byte_len, self.num_hashes) for _ in range(self.num_tables)
-        ]
+        if planes is None:
+            self.planes = [
+                np.random.randn(self.hash_byte_len, self.num_hashes)
+                for _ in range(self.num_tables)
+            ]
+        else:
+            self.planes = planes
 
         self.hash_tables: List[Dict] = [dict() for i in range(self.num_tables)]
 
@@ -30,11 +40,15 @@ class LSH:
 
     def query(self, vector: List, k: int = None, dist_func=euclidean_dist):
         # [y for x in a for y in [x[0]] * x[1]]
-        contestants = []
+        contestants: List[Any] = []
         for loc, htable in enumerate(self.hash_tables):
             this_hash = self.hash(self.planes[loc], vector)
-            contestants.extend(htable.get(this_hash))
-        candidates = [(contestent, dist_func(vector, contestent[0])) for contestent in contestants]
+            contestants.extend(htable.get(this_hash))  # type: ignore
+        candidates = [
+            (contestent, dist_func(vector, contestent[0])) for contestent in contestants
+        ]
         candidates.sort(key=lambda x: x[1])
-        candidate_data = list(OrderedDict.fromkeys([candidate[0][1] for candidate in candidates]))
+        candidate_data = list(
+            OrderedDict.fromkeys([candidate[0][1] for candidate in candidates])
+        )
         return candidate_data[:k] if k else candidate_data
